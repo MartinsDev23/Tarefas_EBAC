7c9eb2b11f1475b519c5a5fcfa0d851d603474cf
Daniel Martins <danmartins016@gmail.com>
2024-10-19 22:45:50 -0300

    Tarefa grunt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('form-sorteador').addEventListener('submit', function(e){
-        e.preventDefault();
-        let numeroMaximo = document.getElementById('numero-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
+// Exemplos
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1);
-        
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
-        document.querySelector('.resultado').style.display = 'block';
-    })
-})
+console.log(
+    'Olá, Mundo!'
+)
