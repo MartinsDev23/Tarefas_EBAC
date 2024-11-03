6025c509696dacf39bdc4d8f4b2dc1fe2cc0829a
Daniel Martins <danmartins016@gmail.com>
2024-11-03 19:58:35 -0300

    Primeira etapa
    Primeira etapa do projeto concluído

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
+function compilaSass() {
+    return gulp
+    .src('./src/main.scss')
+    .pipe(sass(
+        { outputStyle: 'compressed' }
+    ))
+    .pipe(gulp.dest('./dist/styles'));
+}
+
+function comprimeImagens() {
+    return gulp
+    .src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.sass = compilaSass;
+exports.images = comprimeImagens;
+
+exports.default = function() {
+    gulp.watch('./src/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+}
+
