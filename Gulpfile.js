bc89db27fd4aea3acca865581ed913a5b8aeb1b1
Daniel Martins <danmartins016@gmail.com>
2024-11-10 15:26:17 -0300

    Projeto finalizado
    Tarefa de landing page finalizado

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function compilaSass() {
     return gulp
-    .src('./src/main.scss')
+    .src('./src/styles/main.scss')
     .pipe(sass(
         { outputStyle: 'compressed' }
     ))
@@ -18,11 +19,20 @@ function comprimeImagens() {
     .pipe(gulp.dest('./dist/images'));
 }
 
+function comprimeJs() {
+    return gulp
+    .src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'));
+}
+
 exports.sass = compilaSass;
 exports.images = comprimeImagens;
+exports.js = comprimeJs;
 
 exports.default = function() {
-    gulp.watch('./src/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
     gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens));
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJs));
 }
 
