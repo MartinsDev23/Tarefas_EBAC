f1e6853d9960cb38991105488f803d23d47c6d4c
Daniel Martins <danmartins016@gmail.com>
2024-11-28 18:48:37 -0300

    Tarefa concluída
    Consegui com a ajuda do tutor do curso a requisitar as informações da API e manipula-las.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import { Token } from "./config.js";
-
 document.addEventListener('DOMContentLoaded', function(){
     
     const imageAvatar = document.getElementById('image-avatar');
@@ -10,41 +8,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const following = document.getElementById('following');
     const linkProfile = document.getElementById('link-profile');
 
-    async function consumirAPI() {
-        const url = 'https://api.github.com/users/MartinsDev23';
-
-        try {
-            const res = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${Token}`,
-                    'Accept': 'application/vnd.github.v3+json'
-                }
-            });
-
-            if (res.ok) {
-                console.log('Tudo certo com a API !')
-            }
-            else {
-                throw new Error (res.status);
-            }
-            
-            const data = await res.json();
-
-            imageAvatar.src = data.avatar_url
-            nome.textContent = data.name
-            username.textContent = data.login
-            repo.textContent = data.public_repos
-            followers.textContent = data.followers
-            following.textContent = data.following
-            linkProfile.innerHTML = `<a href="${data.html_url}" id="link-profile" class="profile-link">Ver no Github</a>`
-            
-        } 
-            
-            catch (error) {
-                console.error('Erro ao consumir a API:', error);
-            }
-    }
-
-    consumirAPI();
+    fetch("https://api.github.com/users/MartinsDev23")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(json){
+        nome.innerText = json.name;
+        imageAvatar.src = json.avatar_url;
+        username.innerText = json.login;
+        repo.innerText = json.public_repos;
+        followers.innerText = json.followers;
+        following.innerText = json.following;
+        linkProfile.innerHTML = `<a href="${json.html_url}" id="link-profile" class="profile-link">Ver no Github</a>`
+    })
 })
+
