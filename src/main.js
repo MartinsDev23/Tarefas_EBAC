742f633da38ba4c237ada11ad033ee597aa88aba
Daniel Martins <danmartins016@gmail.com>
2024-12-01 17:55:10 -0300

    Tarefa concluída
    A tarefa consiste em criar 2 funções, onde 1 retorna uma multiplicação e a outra uma concatenação, usando o typescript

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaValores(valor1, valor2) {
+    return (valor1 * valor2);
+}
+console.log(multiplicaValores(10, 5));
+function dizOla(nome) {
+    return console.log(`Olá ${nome}`);
+}
+dizOla('Daniel');
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicaValores(valor1: number, valor2:number) {
+    return (valor1 * valor2);
+}
+
+console.log(multiplicaValores(10, 5));
+
+function dizOla(nome:string) {
+    return console.log(`Olá ${nome}`);
+}
+
+dizOla('Daniel');
